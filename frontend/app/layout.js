9e16eab578e8f48dc1c2340d4a43c7709521ec31
Deepak Gudi <18bcar4007@jainuniversity.ac.in>
2026-05-10 14:25:25 +0530

    hydration warnings

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   assertFrontendEnv();
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
